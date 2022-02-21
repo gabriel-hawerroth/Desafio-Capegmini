1eac8ddc4902ce461e3c58a4f44569afb0532cb3
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2022-02-20 23:05:59 -0300

    Update Escada.java

--- Escada.java
+++ Escada.java
@@ -8,20 +8,20 @@ public class Escada {
         /* Define o tamanho da escada, dando um limite máximo de linhas a serem feitas 
         (o limite é declarado com o int 'degraus') */
         for(int i = 1; i <= degraus; i++) {
-            /* Incrementa os espaços e asteriscos na string escada */
+            // Incrementa os espaços e asteriscos na string escada
             for(int e = 1; e <= espacos; e++) {
                 escada = escada + " ";
             }
             for(int d = 1; d <= lajota; d++) {
                 escada = escada + "*";
             }
-            /* Acrescenta 1 na lajota e decrementa 1 espaco, para formar a escada */
+            // Acrescenta 1 na lajota e decrementa 1 espaco, para formar a escada
             lajota = lajota + 1;
             espacos = espacos - 1;
 
-            /* Imprime o resultado da string escada */
+            // Imprime o resultado da string escada
             System.out.println(escada);
-            /* Zera a string escada */
+            // Zera a string escada
             escada = "";
         }
     }
